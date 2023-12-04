d37c323e6fd6f24b9411a6e2d81c88e2409a5446
yimhb <57057194+yimhb@users.noreply.github.com>
2023-12-04 18:00:49 +0900

    login.php

--- login.php
+++ login.php
@@ -1,41 +1,28 @@
 <?php
 session_start();
 
-// 데이터베이스에 연결
-$host = "localhost";
-$username = "사용자명";
-$password = "비밀번호";
-$dbname = "데이터베이스명";
-
-// 연결 생성
-$conn = new mysqli($host, $username, $password, $dbname);
-
-// 연결 확인
-if ($conn->connect_error) {
-    die("연결 실패: " . $conn->connect_error);
-}
+// 하드코딩된 사용자명과 비밀번호
+$correct_username = "testuser";
+$correct_password = "testpass";
 
 // 폼이 제출되었는지 확인
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $user = mysqli_real_escape_string($conn, $_POST['username']);
-    $pass = mysqli_real_escape_string($conn, $_POST['password']);
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
 
     // 자격증명 확인
-    $query = "SELECT * FROM users WHERE username='$user' AND password='$pass'";
-    $result = $conn->query($query);
-
-    if ($result->num_rows > 0) {
+    if ($user === $correct_username && $pass === $correct_password) {
         // 자격증명이 맞으면
         $_SESSION['loggedin'] = true;
         $_SESSION['username'] = $user;
         // 새 페이지로 리디렉션
         header("location: welcome.php");
+        exit;
     } else {
         // 자격증명이 틀리면
         $error = "로그인 이름 또는 비밀번호가 유효하지 않습니다";
     }
 }
-$conn->close();
 ?>
 
 <!DOCTYPE html>
